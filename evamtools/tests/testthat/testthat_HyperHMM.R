3bc21c5633626a1072f6d14f2794c5655e5aef59
delaconcepcion.carlota <delaconcepcion.carlota@gmail.com>
2026-01-02 18:04:57 +0100

    HyperHMM transition matrix check (Lucia) and Check matrix dimensions (MartaR)

--- evamtools/tests/testthat/testthat_HyperHMM.R
+++ evamtools/tests/testthat/testthat_HyperHMM.R
@@ -34,3 +34,55 @@ test_that("Transforming for binary to integer",{
     }
 )
 
+test_that("Check matrix dimensions",{
+    set.seed(123) # Cualquier número sirve
+    dataf <- data.frame(
+            a = sample(c(0, 1), 10, replace = TRUE),
+            b = sample(c(0, 1), 10, replace = TRUE),
+            c = sample(c(0,1 ), 10, replace = TRUE))
+    num_features <- ncol(dataf)
+    expected_dim <- 2^num_features
+
+    r_evam <- evam(dataf, method="HyperHMM")
+
+    expect_equal(nrow(r_evam$HyperHMM_trans_mat),expected_dim)
+    expect_equal(ncol(r_evam$HyperHMM_trans_mat),expected_dim)
+})
+
+test_that("HyperHMM transition matrix check", {
+    set.seed(123)
+    m <- data.frame(
+    a = sample(c(0, 1), 10, replace = TRUE),
+    b = sample(c(0, 1), 10, replace = TRUE),
+    c = sample(c(0,1 ), 10, replace = TRUE)
+    )
+    
+    r <- evam(m, method = "HyperHMM")
+    
+    # Accedemos a la matriz de transición (trans_mat o transition_rate_matrix)
+    t_mat <- r$trans_matrix
+
+    expect_s4_class(t_mat, "dgCMatrix") # Verifica que es una dgCMatrix
+
+    states <- c("WT", "a", "b", "c", "a, b", "a, c", "b, c", "a, b, c")
+    exp_mat <- matrix(0, nrow = 8, ncol = 8, dimnames = list(states, states))
+    
+    exp_mat["WT", "a"] <- 0.3364114
+    exp_mat["WT", "b"] <- 0.4957018
+    exp_mat["WT", "c"] <- 0.1678868
+
+    exp_mat["a", "a, c"] <- 1.0000000
+    exp_mat["a", "a, b"] <- 5.795146e-11 
+    exp_mat["b", "b, c"] <- 1.0000000
+    exp_mat["b", "a, b"] <- 9.166581e-10
+    exp_mat["c", "a, c"] <- 0.8961278
+    exp_mat["c", "b, c"] <- 0.1038722
+    
+    exp_mat["a, b", "a, b, c"] <- 1
+    exp_mat["a, c", "a, b, c"] <- 1
+    exp_mat["b, c", "a, b, c"] <- 1
+    
+    expect_equal(t_mat, exp_mat)
+    
+    expect_equal(dim(t_mat), c(8, 8))
+})
